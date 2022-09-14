98e50d80c41918aaf6d8a6e13d4c04a8c23009d3
marhamhafiz <damarham13@gmail.com>
2022-09-15 01:53:49 +0700

    add vercel.json

--- index.js
+++ index.js
@@ -232,3 +232,5 @@ app.post('/deletecontact', (req,res) => {
 });
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000');
+// Export the Express API
+module.exports = app;
